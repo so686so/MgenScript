813bf084fffecc0a2f1d595b79e084a7d3533a2e
SoByungJun <so686so@gmail.com>
2023-11-22 18:33:09 +0900

    [Fix] install_script fix

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,21 +1,4 @@
-
-function _get_more_recent_edit_file_() { # determine which of two files was most recently modified
-    local _file_1=$1
-    local _file_2=$2
-
-    if [[ -e "$_file_1" && -e "$_file_2" ]]; then
-        # Get the modification time in seconds since the epoch for each file
-        local _time_1=$(stat -c %Y "$_file_1")
-        local time_2=$(stat -c %Y "$_file_2")
-
-        # Compare modification times
-        if   [[ "$_time_1" -gt "$time_2" ]]; then echo "$_file_1"
-        elif [[ "$_time_1" -lt "$time_2" ]]; then echo "$_file_2"
-        else echo "$_file_1"
-        fi
-    fi
-}
-
+#/bin/bash
 # =====================================================================================================================
 
 # Check sudo
@@ -26,22 +9,22 @@ fi
 
 curr_dir_path=$(pwd)
 script_path=$(readlink -f "$0")
+script_dir_path=$(dirname "${script_path}")
 
 update_target_script_file="${script_path}/Scripts/.bash_aliases"
-origin_target_script_file="${HOME}/.bash_aliases"
 update_target_fzf_file="${script_path}/Extensions/fzf_mgen"
 
 if [[ -e "${update_target_script_file}" ]]; then 
-    recent_script="$( _get_more_recent_edit_file_ "${origin_target_script_file}" "${update_target_script_file}" )"
-    if [[ "${recent_script}" == "${update_target_script_file}" ]]; then
-        cp -a "${update_target_script_file}" "${HOME}"
-	source ${HOME}/.bashrc
-    fi
+    cp -a "${update_target_script_file}" "${HOME}"
+    source ${HOME}/.bashrc
 fi
 
 if [[ -e "${update_target_fzf_file}" ]]; then
     cp -a "${update_target_fzf_file}" /usr/bin
 fi
+
+cd ${HOME}
+mv ${script_dir_path} ${HOME}/.MgenScript
 
 echo -e "=============================="
 echo -e "[DONE] Install MgenScript Done"
